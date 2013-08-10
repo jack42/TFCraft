4d804a9af03eb7d0f64b05a10a9ba1e478523562
jack42 <aaue@hrz.tu-chemnitz.de>
2013-08-10 17:50:52 +0200

    move textures TFC Resources/mods/TFC/ -> TFC Resources/assets/terrafirmacraft/

--- TFC_Shared/src/TFC/Items/Tools/ItemCustomBow.java
+++ TFC_Shared/src/TFC/Items/Tools/ItemCustomBow.java
@@ -1,10 +1,11 @@
 package TFC.Items.Tools;
 
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.item.Item;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
@@ -22,11 +23,11 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemCustomBow extends ItemBow implements ISize
-{
-	public static final String[] bowPullIconNameArray = new String[] {"pulling_0", "pulling_1", "pulling_2"};
-	@SideOnly(Side.CLIENT)
-	private Icon[] iconArray;
-    
+{  
+    public static final String[] bowPullIconNameArray = new String[] {"pulling_0", "pulling_1", "pulling_2"};
+    @SideOnly(Side.CLIENT)
+    private Icon[] iconArray;
+
     public ItemCustomBow(int par1)
     {
         super(par1);
@@ -111,7 +112,7 @@ public class ItemCustomBow extends ItemBow implements ISize
             }
         }
     }
-
+  
     /**
      * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
      */
@@ -134,21 +135,18 @@ public class ItemCustomBow extends ItemBow implements ISize
     
 	@Override
 	public EnumSize getSize() {
-		// TODO Auto-generated method stub
 		return EnumSize.LARGE;
 	}
 
 
 	@Override
 	public EnumWeight getWeight() {
-		// TODO Auto-generated method stub
 		return EnumWeight.LIGHT;
 	}
 
 
 	@Override
 	public boolean canStack() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
